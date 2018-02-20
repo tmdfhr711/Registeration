f4f37fe81d2ed2695b4c560cecb9891b55a8e7f5
David.OH <dev_david@naver.com>
2018-02-20 21:38:46 +0900

    6. 메인 화면 구성 및 화면 전환
    1) MainActivity 화면구성
    2) Fragment 생성 및 화면전환 구현

--- app/src/main/java/com/plplim/david/registeration/LoginActivity.java
+++ app/src/main/java/com/plplim/david/registeration/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -41,6 +42,7 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.e("Loginactivity", "로그인버튼");
                 String userID = idText.getText().toString();
                 String userPassword = passwordText.getText().toString();
 
--- app/src/main/java/com/plplim/david/registeration/MainActivity.java
+++ app/src/main/java/com/plplim/david/registeration/MainActivity.java
@@ -1,7 +1,12 @@
 package com.plplim.david.registeration;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,61 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button courseButton = (Button) findViewById(R.id.mainactivity_courseButton);
+        final Button statisticsButton = (Button) findViewById(R.id.mainactivity_statisticsButton);
+        final Button scheduleButton = (Button) findViewById(R.id.mainactivity_scheduleButton);
+        final LinearLayout notice = (LinearLayout) findViewById(R.id.mainactivity_notice);
+
+        courseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //프래그먼트를 보이면서
+                //노티스 숨기기
+                notice.setVisibility(View.GONE);
+                courseButton.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                statisticsButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                scheduleButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                FragmentManager fragmentManager = getSupportFragmentManager();
+
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.mainactivity_fragment, new CourseFragment());
+                fragmentTransaction.commit();
+            }
+        });
+
+        statisticsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //프래그먼트를 보이면서
+                //노티스 숨기기
+                notice.setVisibility(View.GONE);
+                courseButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                statisticsButton.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                scheduleButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                FragmentManager fragmentManager = getSupportFragmentManager();
+
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.mainactivity_fragment, new StatisticsFragment());
+                fragmentTransaction.commit();
+            }
+        });
+
+        scheduleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //프래그먼트를 보이면서
+                //노티스 숨기기
+                notice.setVisibility(View.GONE);
+                courseButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                statisticsButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                scheduleButton.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                FragmentManager fragmentManager = getSupportFragmentManager();
+
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.mainactivity_fragment, new ScheduleFragment());
+                fragmentTransaction.commit();
+            }
+        });
     }
 }
